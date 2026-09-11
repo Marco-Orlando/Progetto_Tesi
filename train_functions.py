dcb10eb01ce192462ff1682046d49dc3cfb926d9
Marco Orlando <103572171+Marco-Orlando@users.noreply.github.com>
2026-09-11 11:58:39 +0200

    Add files via upload

--- train_functions.py
+++ train_functions.py
@@ -4,13 +4,15 @@ from tqdm import tqdm
 import os 
 from utility_functions import reconstruct_image
 
-def loss_function(x_rec,x_real,mu,logvar):
+
+                                                ##--------VANILLA_VAE-------##
+
+
+def loss_function(x_rec,x_real,mu,logvar,beta=1.0):
   REC_LOSS=torch.mean(torch.sum(F.mse_loss(x_rec,x_real,reduction='none'),dim=[1, 2, 3]))
   KL_DIV=-0.5*torch.mean(torch.sum(1-logvar.exp()-mu.pow(2)+logvar,dim=1))
-  TOT_LOSS=REC_LOSS+KL_DIV
+  TOT_LOSS=REC_LOSS+beta*KL_DIV
   return TOT_LOSS,REC_LOSS,KL_DIV
-
-
 
 def train_vae(model,data_loader,optimizer,criterion,n_epochs,device,test_image=None,checkpoint_file='checkpoint.pth',print_results=False,time_print=2):
   model.train()
@@ -37,6 +39,67 @@ def train_vae(model,data_loader,optimizer,criterion,n_epochs,device,test_image=N
       #nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
       optimizer.step()
       progress.set_postfix({'Tot_loss': tot_loss.item(), 'Rec_loss': rc_loss.item(), 'KL_div': kl_div.item()})
+      losses+=tot_loss.item()
+    epoch_loss=losses/len(data_loader)
+    checkpoint={
+      'epoch': epoch,
+      'model_state_dict': model.state_dict(),
+      'optimizer_state_dict': optimizer.state_dict(),
+      'loss': tot_loss.item()
+    }
+    print(f'||-- Loss mean: {epoch_loss}')
+    if print_results:
+        if (epoch+1)%time_print==0:
+          reconstruct_image(model,test_image)
+    torch.save(checkpoint,f'{checkpoint_file}')
+    print(f'Modello salvato corretamente ( Epoca: {epoch} )')
+
+##------------BETA_VAE---------------##
+import math
+
+def get_current_beta(current_step,max_beta=10,min_beta=0,steps=10,t_0=5,k=0.005,waiting=1,max_time=1,mode='linear'):
+    if current_step<waiting:
+        current_beta=min_beta
+    elif current_step>max_time:
+        current_beta=max_beta
+    else:
+        if mode=='linear':
+            betas=torch.linspace(min_beta,max_beta,(steps-(waiting+max_time)))
+            current_beta=betas[current_step]
+            current_beta=current_beta.item()
+        elif mode=='sigmoid':
+            sigmid=1/(1+math.exp(-k*(current_step-t_0)))
+            current_beta = max_beta + (max_beta - min_beta) * sigmoid
+        else:
+            current_beta=1.0
+    
+    return current_beta
+
+def train_vae_beta(model,data_loader,optimizer,criterion,n_epochs,device,test_image=None,checkpoint_file='checkpoint_beta.pth',print_results=False,time_print=2):
+  model.train()
+  model.to(device)
+  print(f'Addestramento avviato su: {device}')
+
+  start_epoch=0
+
+  if os.path.exists(checkpoint_file):
+    checkpoint = torch.load(checkpoint_file, map_location=device)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    start_epoch=checkpoint['epoch']+1
+
+  for epoch in range(start_epoch,n_epochs):
+    progress=tqdm(data_loader,desc=f'Epoch:{epoch+1}/{n_epochs}')
+    losses=0.0
+    current_beta=get_current_beta(epoch,steps=n_epochs,max_beta=10,min_beta=0,t_0=(n_epochs/2),k=0.005,mode='linear')
+    for x_batch in progress:
+      x_batch=x_batch.to(device)
+      x_rec,mu,logvar=model(x_batch)
+      tot_loss,rc_loss,kl_div=criterion(x_rec,x_batch,mu,logvar,current_beta)
+      optimizer.zero_grad()
+      tot_loss.backward()
+      optimizer.step()
+      progress.set_postfix({'Tot_loss': tot_loss.item(), 'Rec_loss': rc_loss.item(), 'KL_div': kl_div.item(),'Beta': current_beta})
       losses+=tot_loss.item()
     epoch_loss=losses/len(data_loader)
     checkpoint={
--- utility_functions.py
+++ utility_functions.py
@@ -6,20 +6,112 @@ def denormalize_image(tensor_image):
     return torch.clamp(tensor_image, 0.0, 1.0)
 
 def show_image(image):
-  if image.min() < 0.0 or image.max() > 1.0:
+    if len(image.shape)==4:
+        image=image.squeeze(0)
+    if image.min() < 0.0 or image.max() > 1.0:
         image = denormalize_image(image)
-  image=image.permute(1,2,0).numpy()
-  plt.figure(figsize=(3,3))
-  plt.imshow(image,interpolation='bilinear')
-  plt.axis('off')
-  plt.show()
+    image=image.permute(1,2,0).cpu().numpy()
+    plt.figure(figsize=(3,3))
+    plt.imshow(image,interpolation='bilinear')
+    plt.axis('off')
+    plt.show()
     
 def reconstruct_image(model,image):
-  model.eval()
-  if len(image.shape)==3:
-    image=image.unsqueeze(0)
-  image=image.to(next(model.parameters()).device)
+    model.eval()
+    if len(image.shape)==3:
+        image=image.unsqueeze(0)
+    image=image.to(next(model.parameters()).device)
+    with torch.no_grad():
+        x_rec, _ , _=model(image)
+    show_image(image)
+    show_image(x_rec)
+
+def interpolate_images(model, img1, img2, steps=10, device='cpu'):
+    model.eval()
+    model.to(device)
+    img1 = img1.unsqueeze(0).to(device)
+    img2 = img2.unsqueeze(0).to(device)
+    with torch.no_grad():
+        mu1, _ = model.encode(img1)
+        mu2, _ = model.encode(img2)
+        alphas = torch.linspace(0, 1, steps=steps).to(device)
+        z_interp = torch.zeros((steps, mu1.shape[1])).to(device)
+        for i, alpha in enumerate(alphas):
+            z_interp[i] = (1 - alpha) * mu1 + alpha * mu2
+        generated_images = model.decode(z_interp).cpu()
+    fig, axes = plt.subplots(1, steps, figsize=(2 * steps, 2))
+    for i in range(steps):
+        img = generated_images[i]
+        if img.min() < 0.0 or img.max() > 1.0:
+            img = denormalize_image(img)
+        img_plot = img.permute(1, 2, 0).numpy()
+        axes[i].imshow(img_plot, interpolation='bilinear')
+        axes[i].axis('off')
+        if i == 0:
+            axes[i].set_title("Inizio")
+        elif i == steps - 1:
+            axes[i].set_title("Fine")
+    plt.tight_layout()
+    plt.show()
+
+def perturb_latent(model,z_vector,latent_idx,value):
+  z=z_vector.clone()
+  z[0,latent_idx]+=value
   with torch.no_grad():
-    x_rec, _ , _=model(image)
-  show_image(image.squeeze(0).cpu())
-  show_image(x_rec.squeeze(0).cpu())
+        img_rec = model.decode(z)
+  show_image(img_rec.squeeze(0).cpu())
+
+def encode_image(model,image,device='cpu'):
+    model.eval()
+    model.to(device)
+    image=image.unsqueeze(0).to(device)
+    with torch.no_grad():
+        mu,_=model.encode(image)
+    return mu
+
+def compute_latent_statistics(model, dataloader, device, num_batches=10):
+    model.eval()
+    model.to(device)
+    all_mus = []
+    with torch.no_grad():
+        for i, batch in enumerate(dataloader):
+            if i >= num_batches:
+                break
+            mu, _ = model.encode(batch.to(device))
+            all_mus.append(mu)
+    all_mus = torch.cat(all_mus, dim=0)
+    mu_mean = torch.mean(all_mus, dim=0)
+    std_mean = torch.std(all_mus, dim=0)
+    return mu_mean.cpu(), std_mean.cpu()
+
+def test_all_latents(model, z_vector,mu_mean=None,std_mean=None, start=-3, end=3, n_steps=10,device='cpu'):
+    model.eval()
+    model.to(device)
+    steps=torch.linspace(start,end,n_steps)
+    n_righe = model.latent_dim
+    n_colonne = len(steps)
+    fig, axes = plt.subplots(n_righe, n_colonne, figsize=(n_colonne * 2, n_righe * 2.2))
+    if n_righe == 1: axes = [axes]
+    if n_colonne == 1: axes = [[ax] for ax in axes]
+    with torch.no_grad():
+        for i in range(n_righe):
+            for j, val in enumerate(steps):
+                z = z_vector.clone().to(device)
+                if mu_mean is None:
+                    z[0, i] +=val
+                else:     
+                    z[0, i] += mu_mean[i] + (stds[i]*val)
+                img_rec = model.decode(z)
+                if isinstance(img_rec, tuple):
+                    img_rec = img_rec[0]
+                img_rec = denormalize_image(img_rec)
+                img_vis = img_rec.squeeze(0).permute(1, 2, 0).cpu().numpy()
+                ax = axes[i][j]
+                ax.imshow(img_vis)
+                ax.axis('off')
+                if i == 0:
+                    ax.set_title(f"Val: {val}")
+                if j == 0:
+                    ax.text(-0.2, 0.5, f"Idx {i}", transform=ax.transAxes, va='center', ha='right', fontsize=12, fontweight='bold')
+    plt.tight_layout()
+    plt.show()
